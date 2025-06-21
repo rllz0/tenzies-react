781ed4e39c2af14ac2742f56879d97116e97fb79
Razan Alqahtani <alqahtaniz.t@stc.com.sa>
2025-06-21 19:46:43 +0300

    Fix MIME type issue for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/tenzies-react/',
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]'
+      }
+    }
+  }
 })
